75ef33a1ecf2b3df3c3a3c5ca0952de19c2e1363
Keshav Rohit <keshavrohit8986@gmail.com>
2026-08-01 17:40:00 +0530

    Add files via upload

---
+++ 01-introduction/01-hello-javascript/02-student-information-card/README.md
@@ -0,0 +1,40 @@
+# Student Information Card
+
+## Description
+
+This is my second JavaScript practice project.
+
+The purpose of this project is to practice declaring variables and displaying information using `console.log()`.
+
+## Concepts Used
+
+- Variables
+- Strings
+- Numbers
+- Console Output
+
+## Project Structure
+
+```
+02-student-information-card/
+│
+├── script.js
+├── README.md
+└── output-student-information-card.png
+```
+
+## Output
+
+![Output](output-student-information-card.png)
+
+## What I Learned
+
+- Declaring variables using `var`
+- Storing different data types
+- Printing formatted output using `console.log()`
+- Organizing a JavaScript project with a proper folder structure
+
+## Future Improvements
+
+- Take user input instead of hardcoded values
+- Display information on a web page using HTML and JavaScript
---
+++ 01-introduction/01-hello-javascript/02-student-information-card/script.js
@@ -0,0 +1,27 @@
+//Student Information Card
+
+var session = "2026-2030";
+var studentId = "SIT2025CS1047";
+var studentName = "Keshav Rohit";
+var fatherName = "Rajesh Kumar Ray";
+var motherName = "Sunita Devi";
+var course = "B.Tech – Computer Science (1st Year)";
+var rollNumber = "CS25-2026";
+var mobileNumber = "+91 22222 55555";
+var address = "Vaishali, Bihar, India";
+
+console.log("========= Student Information Card =========");
+console.log("Sunrise Institute of Technology");
+console.log("Sector 12, Noida, Uttar Pradesh, India");
+console.log("Session       :", session);
+console.log("Student ID    :", studentId);
+console.log("  ");
+console.log("Student Name  :", studentName);
+console.log("Father's Name :", fatherName);
+console.log("Mother's Name :", motherName);
+console.log("Course        :", course);
+console.log("Roll Number   :", rollNumber);
+console.log("Mobile Number :", mobileNumber);
+console.log("Address       :", address);
+console.log("============================================");
+console.log("⚠️ This is fake information for practice only.");
